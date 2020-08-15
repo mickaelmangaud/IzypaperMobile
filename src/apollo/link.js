4685b79e34068c21eb0d92fbfd584fc8caa3a959
Mickael Mangaud <mickaelmangaud@gmail.com>
2020-08-15 13:29:28 +0200

    comment

--- src/apollo/link.js
+++ src/apollo/link.js
@@ -1,6 +1,7 @@
 import { HttpLink } from '@apollo/client';
 
 export const link = new HttpLink({
-  uri: `http://172.20.64.1:5000/graphql`,
-  credentials: 'include',
+    /* "localhost" does not work, see https://github.com/apollographql/apollo-client/issues/1757 */
+    uri: `http://172.20.64.1:5000/graphql`,
+    credentials: 'include',
 });
